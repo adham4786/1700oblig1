4a967cdbec6fe10f56f4d820323d163960fcb34d
adham4786 <143513527+adham4786@users.noreply.github.com>
2024-02-10 18:06:11 +0100

    hope this worked

---
+++ src/main/java/com/example/oblig1web1700/Oblig1Web1700Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig1web1700;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1Web1700Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1Web1700Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig1web1700/Oblig1Web1700ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1web1700;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1Web1700ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
