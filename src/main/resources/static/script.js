c9eb31a6ee0a2c4150c3a9e60bf56133c834828b
adham4786 <143513527+adham4786@users.noreply.github.com>
2024-02-12 11:39:19 +0100

    created js funtion populateHTML

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,12 +1,30 @@
 bilettArray= [];
 
 function leggTilArray(){
-
+    let film = document.getElementById("film").value;
     let antall = document.getElementById("antall").value;
     let fornavn = document.getElementById("fornavn").value;
     let etternavn = document.getElementById("etternavn").value;
     let telefonnr = document.getElementById("telefonnr").value;
     let epost = document.getElementById("epost").value;
-    bilettArray.push({antall:antall,fornavn:fornavn,etternavn:etternavn,
-    telefonnr:telefonnr,epost:epost})
+    bilettArray.push({film:film,antall:antall,fornavn:fornavn,
+        etternavn:etternavn,telefonnr:telefonnr,epost:epost})
+    console.log(bilettArray);
+    populateHTML(bilettArray);
+}
+
+function populateHTML(innArray){
+    console.log("So far so good")
+    let html = "<ul>";
+    console.log(innArray)
+    for(let i in innArray){
+        console.log(innArray[i].fornavn)
+        html += "<li>" + innArray[i].film+"<br>"+innArray[i].antall+"<br>"+
+            innArray[i].fornavn+"<br>"+innArray[i].etternavn+"<br>"+
+            innArray[i].telefonnr+"<br>"+innArray[i].epost+"<br>"+"</li>"
+        html+="</ul>"
+        document.getElementById("bilett").innerHTML = html;
+        console.log(html)
+    }
+
 }
