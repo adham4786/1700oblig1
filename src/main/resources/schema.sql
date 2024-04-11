33a9f67193c044e8ee07d92da8b5f57b5a9aa1b1
adham4786 <143513527+adham4786@users.noreply.github.com>
2024-04-11 14:03:22 +0200

    edited table in sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,21 @@
 CREATE TABLE Kinobillett (
+    billettnr INT AUTO_INCREMENT PRIMARY KEY,
+    film VARCHAR(255) NOT NULL,
+    antall INT NOT NULL,
+    fornavn VARCHAR(255) NOT NULL,
+    etternavn VARCHAR(255) NOT NULL,
+    telefon BIGINT NOT NULL,
+    epost VARCHAR(255) NOT NULL
+);
+
+
+
+
+
+
+
+
+/* CREATE TABLE Kinobillett (
     billettnr INTEGER AUTO_INCREMENT NOT NULL,
     film VARCHAR(255) NOT NULL,
     antall INTEGER(999) NOT NULL,
@@ -7,4 +24,4 @@ CREATE TABLE Kinobillett (
     telefon INTEGER(8) NOT NULL,
     epost VARCHAR(255) NOT NULL,
     PRIMARY KEY (billettnr)
-    );
+    ); */
